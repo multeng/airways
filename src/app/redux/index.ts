b329cfe89001fe7796c29bdc6c2485c789a7f621
Vladimir Kormakov <V.Kormakov@skoltech.ru>
2023-04-20 10:52:14 +0300

    feat: add ngrx

---
+++ src/app/redux/index.ts
@@ -0,0 +1,9 @@
+import { isDevMode } from '@angular/core';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface State {}
+
+export const reducers: ActionReducerMap<State> = {};
+
+export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
