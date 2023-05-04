09d8268dbf0c1d80c23308239d61fd948d8e16fe
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-04 12:57:44 +0300

    refactor/api - add getFlights()

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
+import Flight from '../models/flights.model';
+import Airport from '../models/airports.model';
+import User from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -18,12 +21,26 @@ export class ApiService {
   getUsers() {
     return this.http
       .get('/api/users')
-      .pipe(catchError(this.handleError<any>([])));
+      .pipe(catchError(this.handleError<User[]>([])));
   }
 
   getAirports(title: string, number = 10, next = 0) {
     return this.http
       .get(`/api/airports?title=${title}&number=${number}&skip=${next}`)
-      .pipe(catchError(this.handleError<any>([])));
+      .pipe(catchError(this.handleError<Airport[]>([])));
+  }
+
+  getFlights(
+    startDate: Date,
+    endDate: Date,
+    oneWay: boolean,
+    number = 10,
+    next = 0
+  ) {
+    return this.http
+      .get(
+        `/api/flying?number=${number}&skip=${next}&startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&oneWay=${oneWay}`
+      )
+      .pipe(catchError(this.handleError<Flight[]>([])));
   }
 }
