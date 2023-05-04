98b566a7b07763133ec6027169924e38141fa388
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-04 14:45:01 +0300

    feat/routing - add path for test api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkWithApiComponent } from './api/work-with-api/work-with-api.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'test', component: WorkWithApiComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
