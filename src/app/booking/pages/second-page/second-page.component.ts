e18f2bfd29fac5d22c5191734dc67d5c9cc861db
KirillDemyanenko <demyanenkokg@gmail.com>
2023-05-24 12:19:45 +0300

    feat/app - add footer

---
+++ src/app/booking/pages/second-page/second-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import SecondMenuComponent from '../../second-menu/second-menu.component';
+
+@Component({
+  selector: 'app-second-page',
+  standalone: true,
+  imports: [CommonModule, SecondMenuComponent],
+  templateUrl: './second-page.component.html',
+  styleUrls: ['./second-page.component.scss'],
+})
+export default class SecondPageComponent {}
